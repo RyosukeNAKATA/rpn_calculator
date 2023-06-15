c1a22fef907283b35fc4203addddfb3ac57252fb
Ryosuke NAKATA <ryosuke@RyosukenoMacBook-Pro.local>
2023-06-16 01:51:01 +0900

    add trait for run

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{stdin, BufRead, BufReader};
 
 #[derive(Parser)]
 #[clap(
@@ -25,18 +25,15 @@ fn main() {
     if let Some(path) = opts.formula_file {
         let f = File::open(path).unwrap();
         let reader = BufReader::new(f);
-
         run(reader, opts.verbose);
-        for line in reader.lines() {
-            let line = line.unwrap();
-            println!("{}", line);
-        }
     } else {
-        println!("No file is specified!");
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, opts.verbose);
     }
 }
 
-fn run(reader: BufReader<File>, verbose: bool) {
+fn run<R: BufRead>(reader: R, verbose: bool) {
     for line in reader.lines() {
         let line = line.unwrap();
         println!("{}", line);
